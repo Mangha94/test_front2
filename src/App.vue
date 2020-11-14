855ebf01dd1c3bd4549c78b78d955fb4993693e0
Manghal <lshs19@nate.com>
2020-11-14 19:42:00 +0900

    gdgd

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="true">
+    <h1>Hello!</h1>
+    <h2>Vue.JS</h2>
+    <button @click="test('lshs19')">ㅎㅇㅎㅇ</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'app',
+  methods: {
+    test (str) {
+      console.log(str)
+
+      axios.get('http://localhost:8019/member/detail/lshs19').then(res => {
+        console.log(res)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.h2 {
+  color: white;
+  background-color: black;
+}
+</style>
